f903abc4c8d25afad4f7dc5d6273eb1785d94dce
abdalrahman-alhmouz <abood.alhmouz121@gmail.com>
2021-02-21 14:08:23 +0200

    second comit

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict'
+let express = require('express');
+
+
+
+
+
+let app = express();
+require('dotenv').config();
+
+const PORT =process.env.PORT;
+
+app.listen(PORT,()=>{
+console.log('the app is listening to apoort'+PORT)
+})
+
+ 
